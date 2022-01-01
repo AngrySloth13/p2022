88961b19e5e01ce676c785452bade03e041f12b7
thongal <hongal@gmail.com>
2022-01-01 10:19:02 -0800

    I fixed my programs

--- two_strings_eg7.py
+++ two_strings_eg7.py
@@ -1,9 +1,28 @@
-import sys
 
-def check_balance(str1,str2):
+import sys
+import re
+'''
+thongal@thongal-000608:~/Documents/shri/p2022$::python3 ./two_strings_eg7.py cat "my name is cat but am not cat"
+1
+2
+'''
+def is_balanced(needle, haystack):
+    pattern = re.compile(needle)
+    sentence = (haystack)
+    matches = pattern.finditer(sentence)
+
+    matching=0
     
+    for match in matches:
+        matching=matching+1
+        print(matching)
+        
+        
+
 def main(argv):
-    check_balance(argv[1],argv[2])
+    is_balanced(argv[1],argv[2])
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
+
+             
